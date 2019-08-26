895673a376a56a72f759173682680ad900610a75
adrianwit@gmail.com <Ubc!1234F>
2019-08-26 12:30:50 -0700

    added json payload auth option

--- gs/jwt.go
+++ gs/jwt.go
@@ -69,7 +69,7 @@ func NewJwtConfig(options ...storage.Option) (*JwtConfig, error) {
 			return nil, errors.Wrap(err, "failed to open auth config")
 		}
 		defer func() { _ = file.Close() }()
-		if JSONPayload, err = ioutil.ReadAll(reader);err != nil {
+		if JSONPayload, err = ioutil.ReadAll(file);err != nil {
 			return nil, err
 		}
 
--- s3/auth.go
+++ s3/auth.go
@@ -53,7 +53,7 @@ func NewAuthConfig(options ...storage.Option) (*AuthConfig, error) {
 			return nil, errors.Wrap(err, "failed to open auth config")
 		}
 		defer func() { _ = file.Close() }()
-		if JSONPayload, err = ioutil.ReadAll(reader);err != nil {
+		if JSONPayload, err = ioutil.ReadAll(file);err != nil {
 			return nil, err
 		}
 
