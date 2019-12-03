d1139e7b02ec2c5f565ff43e124d3390625ba5be
adrianwit@gmail.com <Ubc!1234F>
2019-12-02 20:36:14 -0800

    added storage retries codes

--- gs/helper.go
+++ gs/helper.go
@@ -5,6 +5,7 @@ import (
 	"google.golang.org/api/googleapi"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -12,13 +13,24 @@ const notFound = "Not Found"
 const storageClassFragment = "storageclass"
 const encryptionFragment = "encryption"
 const backendError = "backendError"
+const backendErrorCode = 10
+const connectionResetCode = 11
+
+
+var errors = make(map[int]int)
+var mux = &sync.Mutex{}
+
 
 //isRetryError returns true if backend error
 func isRetryError(err error) bool {
 	if err == nil {
 		return false
 	}
+
 	if apiError, ok := err.(*googleapi.Error); ok {
+		mux.Lock()
+		errors[apiError.Code]++
+		mux.Unlock()
 		if apiError.Code == http.StatusServiceUnavailable {
 			return true
 		}
@@ -26,8 +38,22 @@ func isRetryError(err error) bool {
 			return true
 		}
 	}
+
 	message := err.Error()
-	return strings.Contains(message, backendError) || strings.Contains(message, "connection reset")
+	if strings.Contains(message, "connection reset") {
+		mux.Lock()
+		errors[connectionResetCode]++
+		mux.Unlock()
+		return true
+	}
+
+	if  strings.Contains(message, backendError) {
+		mux.Lock()
+		errors[connectionResetCode]++
+		mux.Unlock()
+		return true
+	}
+	return false
 }
 
 func isBucketNotFound(err error) bool {
@@ -70,4 +96,15 @@ func isNotFound(err error) bool {
 		}
 	}
 	return strings.Contains(err.Error(), notFound)
+}
+
+
+func GetRetryCodes(reset bool) map[int]int {
+	result :=  errors
+	if reset {
+		mux.Lock()
+		errors = make(map[int]int)
+		mux.Unlock()
+	}
+	return result
 }
