e21b75a06c69900a818467a0dce6209eede3ff3c
Adrian Witas <adrianwit@gmail.com>
2021-06-07 15:53:44 -0700

    removed debug

--- s3/copy.go
+++ s3/copy.go
@@ -8,15 +8,12 @@ import (
 	"github.com/pkg/errors"
 	"github.com/viant/afs/option"
 	"github.com/viant/afs/storage"
-	"github.com/viant/toolbox"
-	"os"
 	"path"
 	"strings"
 )
 
 const (
 	maxCopySize = 5  * 1024 * 1024 * 1024
-	maxMultiCopySizeThresholdMBKey = "AWS_MCOPY_THRESHOLD_MB"
 )
 
 func (s *storager) Copy(ctx context.Context, sourcePath, destBucket, destPath string, options ...storage.Option) error {
@@ -55,11 +52,7 @@ func (s *storager) Copy(ctx context.Context, sourcePath, destBucket, destPath st
 		Key:        &destPath,
 		Bucket:     &destBucket,
 	}
-	maxSize := int64(maxCopySize)
-	if value := os.Getenv(maxMultiCopySizeThresholdMBKey);value != "" {
-		maxSize = int64(toolbox.AsInt(value))
-	}
-	if source.Size() >= maxSize {
+	if source.Size() >= maxCopySize {
 		copyer := newCopyer(s.S3, source, defaultPartSize, copyInput)
 		return copyer.copy(ctx)
 	}
